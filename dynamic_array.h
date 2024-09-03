1485f11ab635eb12b81b81a4ae08d1e16970b268
Камиль Гимранов <gkr004@campus.mephi.ru>
2024-09-03 19:19:25 +0300

    all algorithms(without quick sort) for dynArr

--- dynamic_array.h
+++ dynamic_array.h
@@ -10,6 +10,42 @@ private:
     }
     size_t size_;
     T *val;
+    //sorting helpers
+
+    template<typename Comparator = std::less<T>>
+    void merge_sort(int left, int right, Comparator comparator) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            merge_sort(left, mid, comparator);
+            merge_sort(mid + 1, right, comparator);
+            merging(comparator, left, mid + 1, mid, right);
+        }
+    }
+
+    template<typename Comparator = std::less<T>>
+    void merging(Comparator comparator, int ptr1, int ptr2, int wall1, int wall2) {
+        int i = 0, ptr = ptr1;
+        DynamicArray<T> Arr;
+        while (ptr1 <= wall1 && ptr2 <= wall2) {
+            Arr.Resize(i + 1);
+            if (comparator(val[ptr1], val[ptr2])) {
+                Arr[i++] = val[ptr1++];
+            } else {
+                Arr[i++] = val[ptr2++];
+            }
+        }
+        while (ptr1 <= wall1) {
+            Arr.Resize(i + 1);
+            Arr[i++] = val[ptr1++];
+        }
+        while (ptr2 <= wall2) {
+            Arr.Resize(i + 1);
+            Arr[i++] = val[ptr2++];
+        }
+        for (int z = 0; z < i; ++z) {
+            val[ptr++] = Arr[z];
+        }
+    }
 public:
     DynamicArray() : size_(0), val(new T[size_]) {}
 
@@ -51,7 +87,6 @@ public:
         }
         size_ = newSize;
     }
-
     const T &operator[](const size_t i) const {
         return val[i];
     }
@@ -59,5 +94,64 @@ public:
     T &operator[](const size_t i) {
         return val[i];
     }
+
+    //SORTING
+    //bubble sort
+    template<typename Comparator = std::less<T>>
+    void bubble_sort(Comparator comparator) {
+        for (int j = 1; j < size_; j++) {
+            bool isSorted = true;
+            for (int i = 0; i < size_ - j; i++) {
+                if (!comparator(val[i], val[i + 1])) {
+                    int tmp = val[i];
+                    val[i] = val[i + 1];
+                    val[i + 1] = tmp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted) {
+                break;
+            }
+        }
+    }
+
+    //shaker sort
+    template<typename Comparator = std::less<T>>
+    void shacker_sort(Comparator comparator) {
+        bool swapped = true;
+        int start = 0;
+        int end = size_ - 1;
+
+        while (swapped) {
+            swapped = false;
+            for (int i = start; i < end; ++i) {
+                if (!comparator(val[i], val[i + 1])) {
+                    std::swap(val[i], val[i + 1]);
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+            swapped = false;
+            --end;
+            for (int i = end - 1; i >= start; --i) {
+                if (!comparator(val[i], val[i + 1])) {
+                    std::swap(val[i], val[i + 1]);
+                    swapped = true;
+                }
+            }
+            ++start;
+        }
+    }
+
+
+    //Merge sort
+
+    template<typename Comparator = std::less<T>>
+    void merge_sort(Comparator comparator) {
+        merge_sort(0, size_ - 1, comparator);
+    }
+
+    //Quick sort
 
 };
--- test_3_1.h
+++ test_3_1.h
@@ -2,9 +2,21 @@
 #include "sorting_algorithms.h"
 void test_3_1(){
     DynamicArray<int> arr({5, 2, 2, 4, 2, 9});
-    bubble_sort(arr, [](int a, int b){return a > b;});
+    DynamicArray<int> arr1 = arr;
+    DynamicArray<int> arr2 = arr;
+    arr.bubble_sort([](int a, int b){return a > b;});
     for(int i = 0; i < arr.Size(); ++i){
         cout << arr[i] << ' ';
     }
     cout << '\n';
+    arr1.shacker_sort([](int a, int b){return a > b;});
+    for(int i = 0; i < arr1.Size(); ++i){
+        std::cout << arr1[i] << ' ';
+    }
+    std::cout << '\n';
+    arr2.merge_sort([](int a, int b){return a > b;});
+    for(int i = 0; i < arr2.Size(); ++i){
+        std::cout << arr2[i] << ' ';
+    }
+    std::cout << '\n';
 }
